ffade5b233820b6b339055524fe92578d635a649
Li <lir@ES.AD.ADP.COM>
2017-09-29 10:52:37 -0400

    added httpclient

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,32 +1,35 @@
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+
 import { AuthService } from '../auth/auth.service';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: Http,
+  constructor(private httpClient: HttpClient,
               private recipeService: RecipeService,
               private authService: AuthService) { }
 
   storeRecipes() {
     const token = this.authService.getToken();
 
-    return this.http.put('https://recipe-book-2c35d.firebaseio.com/recipes.json?auth=' + token,
+    return this.httpClient.put('https://recipe-book-2c35d.firebaseio.com/recipes.json?auth=' + token,
                     this.recipeService.getRecipes());
   }
 
   getRecipes() {
     const token = this.authService.getToken();
 
-    this.http.get('https://recipe-book-2c35d.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>('https://recipe-book-2c35d.firebaseio.com/recipes.json?auth=' + token)
             .map(
-              (response: Response) => {
-                const recipes: Recipe[] = response.json();
+              // (response: Response) => {
+                // const recipes: Recipe[] = response.json();
+                (recipes) => {
                 for (let recipe of recipes) {
                   if (!recipe['ingredients']) {
                     console.log(recipe); // check if there's no ingredients for that recipe
